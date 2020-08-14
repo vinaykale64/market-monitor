cbd09e8738c610df5d4023dfa76af6519bbc0b1f
Vinay <vinaykale64@gmail.com>
2020-08-13 22:33:24 -0400

    black

--- scripts/functions.py
+++ scripts/functions.py
@@ -23,50 +23,55 @@ def stock_chart(stock_object, period):
 
     """
 
-    period_list = ['1d', '1mo', '3mo', '1y', '2y', '5y', '10y', 'ytd', 'max']
+    period_list = ["1d", "1mo", "3mo", "1y", "2y", "5y", "10y", "ytd", "max"]
     if period not in period_list:
-        raise KeyError("Incorrect period specified. Allowed list: "
-                       "['1d','1mo','3mo','1y','2y','5y','10y','ytd','max']")
+        raise KeyError(
+            "Incorrect period specified. Allowed list: "
+            "['1d','1mo','3mo','1y','2y','5y','10y','ytd','max']"
+        )
 
     # Intervals set similar to RH policy
-    if period == '1d':
-        interval = '5m'
+    if period == "1d":
+        interval = "5m"
     else:
-        interval = '1d'
+        interval = "1d"
 
     data = stock_object.history(period=period, interval=interval)
-    plot_title = 'Stock: {}, Time Period: {}'.format(stock_object.ticker, period)
+    plot_title = "Stock: {}, Time Period: {}".format(stock_object.ticker, period)
 
     fig = go.Figure(
-        data=go.Scatter(y=data['Close'],
-                        x=data.index,
-                        line=dict(color='chartreuse', width=4)
-                        ),
-
+        data=go.Scatter(
+            y=data["Close"], x=data.index, line=dict(color="chartreuse", width=4)
+        ),
     )
 
     fig.add_layout_image(
         dict(
-            source=stock_object.info['logo_url'],
-            xref="paper", yref="paper",
-            x=1, y=1.05,
-            sizex=0.2, sizey=0.2,
-            xanchor="left", yanchor="bottom"
+            source=stock_object.info["logo_url"],
+            xref="paper",
+            yref="paper",
+            x=1,
+            y=1.05,
+            sizex=0.2,
+            sizey=0.2,
+            xanchor="left",
+            yanchor="bottom",
         )
     )
 
-    fig.update_layout(title=plot_title,
-                      hovermode="x",
-                      yaxis_title='Closing Price',
-                      template='plotly_dark',
-                      xaxis_showgrid=False,
-                      yaxis_showgrid=False
-                      )
+    fig.update_layout(
+        title=plot_title,
+        hovermode="x",
+        yaxis_title="Closing Price",
+        template="plotly_dark",
+        xaxis_showgrid=False,
+        yaxis_showgrid=False,
+    )
 
     return fig
 
 
-def options_table(stock_object, date=None, kind='calls'):
+def options_table(stock_object, date=None, kind="calls"):
     """
     Returns the info about options of a stock on any given date in tabular format.
     If `date` is not given, it fetches data for the next closest date. If `kind` is
@@ -96,44 +101,66 @@ def options_table(stock_object, date=None, kind='calls'):
     if date is None:
         date = possible_dates[0]
     if date not in possible_dates:
-        raise KeyError('Invalid date selected. For available dates, try ticker_object.options')
+        raise KeyError(
+            "Invalid date selected. For available dates, try ticker_object.options"
+        )
 
-    if kind == 'calls':
+    if kind == "calls":
         data = stock_object.option_chain(date=date).calls
 
-    elif kind == 'puts':
+    elif kind == "puts":
         data = stock_object.option_chain(date=date).puts
 
     else:
-        raise KeyError('Invalid kind selected. Only `calls` and `puts` supported.')
+        raise KeyError("Invalid kind selected. Only `calls` and `puts` supported.")
 
-    data = data[['strike',
-                 'lastPrice',
-                 'bid',
-                 'ask',
-                 'change',
-                 'percentChange',
-                 'volume',
-                 'openInterest',
-                 'impliedVolatility',
-                 'inTheMoney']]
+    data = data[
+        [
+            "strike",
+            "lastPrice",
+            "bid",
+            "ask",
+            "change",
+            "percentChange",
+            "volume",
+            "openInterest",
+            "impliedVolatility",
+            "inTheMoney",
+        ]
+    ]
 
-    data.columns = ['Strike', 'Last Price', 'Bid', 'Ask', 'Change', 'Percent Change',
-                    'Volume', 'Open Interest', 'Implied Volatility', 'In The Money']
+    data.columns = [
+        "Strike",
+        "Last Price",
+        "Bid",
+        "Ask",
+        "Change",
+        "Percent Change",
+        "Volume",
+        "Open Interest",
+        "Implied Volatility",
+        "In The Money",
+    ]
 
     data = data.round(3)
-    bold_columns = ['<b>' + x + '</b>' for x in data.columns]
+    bold_columns = ["<b>" + x + "</b>" for x in data.columns]
 
-    fig = go.Figure(data=[go.Table(
-        columnwidth=[1, 1.1, 1, 1, 1, 1.4, 1.1, 1.4, 1.5, 1.2],
-        header=dict(values=bold_columns,
-                    fill_color='black',
-                    line_color='darkslategray',
-                    font=dict(color='white', size=12),
-                    align='center'),
-        cells=dict(values=data.T.values.tolist(),
-                   fill_color='white',
-                   align='center'))
-    ])
+    fig = go.Figure(
+        data=[
+            go.Table(
+                columnwidth=[1, 1.1, 1, 1, 1, 1.4, 1.1, 1.4, 1.5, 1.2],
+                header=dict(
+                    values=bold_columns,
+                    fill_color="black",
+                    line_color="darkslategray",
+                    font=dict(color="white", size=12),
+                    align="center",
+                ),
+                cells=dict(
+                    values=data.T.values.tolist(), fill_color="white", align="center"
+                ),
+            )
+        ]
+    )
 
     return fig
