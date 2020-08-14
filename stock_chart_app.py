edefd0b16f5ce74aef8d1cb003e26502044e1b1e
Vinay <vinaykale64@gmail.com>
2020-08-13 22:49:14 -0400

    added dropdown for options possible dates to app

--- stock_chart_app.py
+++ stock_chart_app.py
@@ -4,74 +4,96 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 
 import yfinance as yf
-from scripts.functions import stock_chart
-import json
+from scripts.functions import stock_chart, options_table
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 
-period_list = ['1d', '1mo', '3mo', '1y', '2y', '5y', '10y', 'ytd', 'max']
-colors = {
-    'background': '#FFFFFF',
-    'text': '#000000'
-}
+period_list = ["1d", "1mo", "3mo", "1y", "2y", "5y", "10y", "ytd", "max"]
+colors = {"background": "#FFFFFF", "text": "#000000"}
 
-app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
-    html.H4("STOCK INFO VISUALIZER",
-            style={'textAlign': 'center', 'color': colors['text']}
-            ),
-    html.Div(["Enter Stock Ticker Symbol ",
-              dcc.Input(id='ticker', value='AAPL', type='text')
-              ],
-             style={'color': colors['text'], 'textAlign': 'center'}
-             ),
-    html.Br(),
-    html.Div(["Select Time Period",
-              dcc.Dropdown(
-                  id='period',
-                  options=[{'label': i, 'value': i} for i in period_list],
-                  value='3mo'
-              )
-            ], style={'width': '20%',
-                      'color': colors['text'],
-                      'textAlign': 'left',
-                      'align-items': 'center',
-                      'padding-left':'40%',
-                      }),
-    html.Div(id='hover-data', style={'color': colors['text'], 'fontSize': 32, 'padding-left':'5%'}),
-    dcc.Graph(id='stock-chart'),
+app.layout = html.Div(
+    style={"backgroundColor": colors["background"]},
+    children=[
+        html.H4(
+            "STOCK INFO VISUALIZER",
+            style={"textAlign": "center", "color": colors["text"]},
+        ),
+        html.Div(
+            [
+                "Enter Stock Ticker Symbol ",
+                dcc.Input(id="ticker", value="AAPL", type="text"),
+            ],
+            style={"color": colors["text"], "textAlign": "center"},
+        ),
+        html.Br(),
+        html.Div(
+            [
+                "Select Time Period",
+                dcc.Dropdown(
+                    id="period",
+                    options=[{"label": i, "value": i} for i in period_list],
+                    value="3mo",
+                ),
+            ],
+            style={
+                "width": "20%",
+                "color": colors["text"],
+                "textAlign": "left",
+                "align-items": "center",
+                "padding-left": "40%",
+            },
+        ),
+        html.Div(
+            id="hover-data",
+            style={"color": colors["text"], "fontSize": 32, "padding-left": "5%"},
+        ),
+        dcc.Graph(id="stock-chart"),
+        html.Br(),
+        dcc.Dropdown(id='dates-dropdown'),
+    ],
+)
 
-])
+@app.callback(
+    dash.dependencies.Output('dates-dropdown', 'options'),
+    [dash.dependencies.Input('ticker', 'value')])
+def get_possible_dates(ticker):
+    obj = yf.Ticker(ticker)
+    return [{'label': i, 'value': i} for i in obj.options]
 
-
+@app.callback(
+    dash.dependencies.Output('dates-dropdown', 'value'),
+    [dash.dependencies.Input('ticker', 'value')])
+def get_default_dates(ticker):
+    obj = yf.Ticker(ticker)
+    return obj.options[0]
 
 
 @app.callback(
-    Output('stock-chart', 'figure'),
-    [Input('ticker', 'value'),
-     Input('period', 'value')])
+    Output("stock-chart", "figure"),
+    [Input("ticker", "value"), Input("period", "value")],
+)
 def update_figure(ticker, period):
 
     obj = yf.Ticker(ticker)
     fig = stock_chart(stock_object=obj, period=period)
     fig.update_layout(
-        plot_bgcolor=colors['background'],
-        paper_bgcolor=colors['background'],
-        font_color=colors['text']
+        plot_bgcolor=colors["background"],
+        paper_bgcolor=colors["background"],
+        font_color=colors["text"],
     )
     return fig
 
-@app.callback(
-    Output('hover-data', 'children'),
-    [Input('stock-chart', 'hoverData')])
+
+@app.callback(Output("hover-data", "children"), [Input("stock-chart", "hoverData")])
 def display_hover_data(hoverData):
     if hoverData is None:
-        return '_'
+        return "_"
     else:
-        return '{}'.format(hoverData['points'][0]['y'], hoverData['points'][0]['x'])
+        return "{}".format(hoverData["points"][0]["y"], hoverData["points"][0]["x"])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run_server(debug=True)
